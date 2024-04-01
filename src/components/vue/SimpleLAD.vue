bd497964cf47043d7e88d57730404443187b50f2
Mazzella, Jesse D. (ARC-TI)[KBR Wyle Services, LLC] <jesse.d.mazzella@nasa.gov>
2024-04-01 16:56:20 -0700

    WIP: some more progress towards sample LAD

--- src/components/vue/SimpleLAD.vue
+++ src/components/vue/SimpleLAD.vue
@@ -5,16 +5,12 @@
         <tr>
           <th>Parameter</th>
           <th>Value</th>
-          <th>Limit</th>
-          <th>Status</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="param in ladData" :key="param.key">
-          <td>{{ param.name }}</td>
-          <td>{{ param.value }}</td>
-          <td>{{ param.limit }}</td>
-          <td :class="getStatusClass(param.status)">{{ param.status }}</td>
+        <tr>
+          <td>{{ name }}</td>
+          <td>{{ value }}</td>
         </tr>
       </tbody>
     </table>
@@ -23,45 +19,73 @@
 
 <script>
 export default {
-  inject: ['openmct', 'domainObject'],
+  inject: ["openmct", "domainObject"],
   data() {
     return {
-      ladData: []
+      datum: null,
+    };
+  },
+  computed: {
+    value() {
+      return this.datum ? this.formats[this.valueKey].format(this.datum) : '---';
+    },
+    name() {
+      return this.datum?.name ?? '---';
     }
   },
   mounted() {
+    const compositionCollection = this.openmct.composition.get(
+      this.domainObject
+    );
+    compositionCollection.on("add", this.addTelemetrySource);
+    compositionCollection.on("remove", this.removeTelemetrySource);
+    compositionCollection.load();
     // Subscribe to telemetry data from OpenMCT
-    this.subscription = this.openmct.telemetry.subscribe(this.domainObject, this.handleTelemetryData)
+    this.subscription = this.openmct.telemetry.subscribe(
+      this.domainObject,
+      this.handleTelemetryData
+    );
   },
   beforeUnmount() {
     // Unsubscribe from telemetry data when the component is destroyed
     if (this.subscription) {
-      this.subscription()
+      this.subscription();
     }
   },
   methods: {
-    handleTelemetryData(data) {
+    addTelemetrySource(domainObject) {
+      
+      this.metadata = this.openmct.telemetry.getMetadata(domainObject);
+      this.formats = this.openmct.telemetry.getFormatMap(this.metadata);
+      if (this.metadata) {
+        this.valueMetadata = this.metadata.valuesForHints(["range"])[0];
+      }
+      this.valueKey = this.valueMetadata ? this.valueMetadata.key : undefined;
+
+      this.subscription = this.openmct.telemetry.subscribe(
+        domainObject,
+        this.handleTelemetryData
+      );
+    },
+    removeTelemetrySource(domainObject) {
+      this.subscription();
+    },
+    handleTelemetryData(datum) {
       // Update the LAD data with the received telemetry data
-      this.ladData = data.map(item => ({
-        key: item.key,
-        name: item.name,
-        value: item.value,
-        limit: item.limit,
-        status: this.getStatus(item.value, item.limit)
-      }))
+      this.datum = datum;
     },
     getStatus(value, limit) {
       // Determine the status based on the value and limit
       if (value > limit) {
-        return 'Out of Limit'
+        return "Out of Limit";
       } else {
-        return 'Normal'
+        return "Normal";
       }
     },
     getStatusClass(status) {
       // Return the CSS class based on the status
-      return status === 'Out of Limit' ? 'out-of-limit' : 'normal'
-    }
-  }
-}
+      return status === "Out of Limit" ? "out-of-limit" : "normal";
+    },
+  },
+};
 </script>
